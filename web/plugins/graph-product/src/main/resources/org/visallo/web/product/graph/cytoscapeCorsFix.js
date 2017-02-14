a64099e6423e1e12bc7ff1f16dae21cdcefef6e7
joeybrk372 <joeybrk372@gmail.com>
2017-02-14 16:50:26 -0500

    Upgrade Cytoscape from 2.7.11 to 2.7.14
    * Issue with requestAnimationFrame in IE which was resolved in 2.7.12

--- web/plugins/graph-product/src/main/resources/org/visallo/web/product/graph/cytoscapeCorsFix.js
+++ web/plugins/graph-product/src/main/resources/org/visallo/web/product/graph/cytoscapeCorsFix.js
@@ -9,12 +9,15 @@ define(['underscore'], function(_) {
         }
     }
 
-    function fixedGetCachedImage(url, onLoad) {
-        if (arguments.length !== 2) {
-            throw new Error('Expected 2 arguments, maybe cytoscape was upgraded?');
+    function fixedGetCachedImage(url, crossOrigin, onLoad) {
+        if (arguments.length !== 3) {
+            throw new Error('Expected 3 arguments, maybe cytoscape was upgraded?');
         }
         if (!_.isString(url)) {
             throw new Error('Expected string url argument');
+        }
+        if (!_.isObject(crossOrigin)) {
+            throw new Error('Expected object crossOrigin argument');
         }
         if (!_.isFunction(onLoad)) {
             throw new Error('Expected function onLoad argument');
@@ -25,24 +28,28 @@ define(['underscore'], function(_) {
         var cache = imageCache[ url ];
 
         if( cache ){
-            if( !cache.image.complete ){
-                cache.image.addEventListener('load', onLoad);
-            }
+          if( !cache.image.complete ){
+            cache.image.addEventListener('load', onLoad);
+          }
 
-            return cache.image;
+          return cache.image;
         } else {
-            cache = imageCache[ url ] = imageCache[ url ] || {};
-            var image = cache.image = new Image(); // eslint-disable-line no-undef
-            image.addEventListener('load', onLoad);
-            if ((/^(\/\/|http)/).test(url)) {
-                image.crossOrigin = 'Anonymous'; // prevent tainted canvas
-            }
-            image.onerror = function(e){
-                console.warn('Error loading graph image', e)
-            }
-            image.src = url;
+          cache = imageCache[ url ] = imageCache[ url ] || {};
 
-            return image;
+          var image = cache.image = new Image(); // eslint-disable-line no-undef
+          image.addEventListener('load', onLoad);
+
+          // #1582 safari doesn't load data uris with crossOrigin properly
+          // https://bugs.webkit.org/show_bug.cgi?id=123978
+          var dataUriPrefix = 'data:';
+          var isDataUri = url.substring( 0, dataUriPrefix.length ).toLowerCase() === dataUriPrefix;
+          if(!isDataUri) {
+            image.crossOrigin = crossOrigin.value; // prevent tainted canvas
+          }
+
+          image.src = url;
+
+          return image;
         }
     }
 });
